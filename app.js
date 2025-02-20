4516ed63b1f25d4a98d4734c81925e72e938cad8
Jitin Kumar chak <jitin42013@gmail.com>
2025-02-20 16:08:34 +0530

    page3

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const scroll = new LocomotiveScroll({
 
 console.log(typeof LocomotiveScroll);
 
+var elemc = document.querySelector("#elem-container");
+var fixed = document.querySelector("#fixed-image");
+elemc.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+elemc.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
 
-var a = document.querySelector(".elem1")
-var abc =  a.getAttribute("data-image")
-console.log(abc);
+var elems = document.querySelectorAll("#elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
